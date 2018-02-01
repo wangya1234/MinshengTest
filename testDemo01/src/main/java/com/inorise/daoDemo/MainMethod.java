48738c15bf2f76822b865174ac1ba838ef9a050d
wangya1234 <2207878599@qq.com>
2018-02-01 10:40:26 +0800

    add

--- testDemo01/src/main/java/com/inorise/daoDemo/MainMethod.java
+++ testDemo01/src/main/java/com/inorise/daoDemo/MainMethod.java
@@ -8,6 +8,7 @@ public class MainMethod {
 
     public static void main(String[] args) {
         SpringApplication.run(MainMethod.class,args);
+        
     }                                                                                                                                                                                                                 
 
 }
