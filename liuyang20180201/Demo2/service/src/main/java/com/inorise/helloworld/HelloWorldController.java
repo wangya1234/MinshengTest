5531a82522e4691e8ccbf5fae0bbb122d2d43276
wangrangzhi <451155928@qq.com>
2018-02-01 18:18:24 +0800

    Revert "Revert "远程调用eureka和本地调用""
    This reverts commit 5a08a9056c8b55f094a1881fd0c5d62a60a267e2.

--- liuyang20180201/Demo2/service/src/main/java/com/inorise/helloworld/HelloWorldController.java
+++ liuyang20180201/Demo2/service/src/main/java/com/inorise/helloworld/HelloWorldController.java
@@ -1,23 +1,44 @@
 package com.inorise.helloworld;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Logger;
+
 
 @RestController
 public class HelloWorldController {
 
     private final Logger logger = Logger.getLogger(String.valueOf(getClass()));
 
+    @Autowired
+    private DiscoveryClient discoveryClient; //服务发现客户端
+
+    
 
     @RequestMapping("hello")
     public String helloWorld() {
         return "Hello World!";
     }
+    
+    @RequestMapping("info")
+    public String helloWorldinfo() {
+        return "info";
+    }
+    
+    @GetMapping(value = "/add")
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+
 
 }
--- liuyang20180201/Demo2/sso/src/main/java/com/inorise/consumer/controller/ConsumerController.java
+++ liuyang20180201/Demo2/sso/src/main/java/com/inorise/consumer/controller/ConsumerController.java
@@ -1,12 +1,11 @@
 package com.inorise.consumer.controller;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.logging.Logger;
 
 @RestController
 public class ConsumerController {
@@ -16,12 +15,18 @@ public class ConsumerController {
 
     private Logger logger = Logger.getLogger(getClass().toGenericString());
 
-    @RequestMapping("/ribbon-consumer")
-    public String helloConsumer()
-    {
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class);
-        String body = forEntity.getBody();
-        logger.info(body);
-        return body;
+   
+    	@GetMapping(value = "/info")
+        public String info() {
+            return restTemplate.getForEntity("http://micro-weather-eureka-client/info", String.class).getBody();
+        }
+
+    	
+    	@GetMapping(value = "/add")
+        public String add() {
+            return restTemplate.getForEntity("http://micro-weather-eureka-client/add?a=10&b=20", String.class).getBody();
+        }
+
+        
     }
-}
+
